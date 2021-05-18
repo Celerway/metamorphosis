f4b3e2cdafcd19fbd636251b77c9288976495d87
Per Buer <per.buer@gmail.com>
2021-05-18 15:01:41 +0200

    more errors.

--- integration/testutil.go
+++ integration/testutil.go
@@ -48,7 +48,7 @@ func mkBrigeParam(wg *sync.WaitGroup, mqttPort, kafkaPort, healthPort int, topic
 		MqttPort:           mqttPort,
 		MqttTopic:          topic,
 		MqttTls:            false,
-		KafkaBroker:        "0.0.0.0",
+		KafkaBroker:        "127.0.0.1",
 		KafkaPort:          kafkaPort,
 		KafkaTopic:         topic,
 		KafkaWorkers:       1,

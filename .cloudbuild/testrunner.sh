5bd60da859ba4e0723d99a2b9ee17fac4b658528
Per Buer <per.buer@gmail.com>
2021-05-19 10:14:32 +0200

    move the test logic out of docker.

--- .cloudbuild/testrunner.sh
+++ .cloudbuild/testrunner.sh
@@ -8,7 +8,7 @@ apt-get install -y unzip
 wget -q https://github.com/vectorizedio/redpanda/releases/download/v21.5.2/rpk-linux-amd64.zip
 unzip rpk-linux-amd64.zip
 mv rpk /usr/local/bin
-rpk topic list
+rpk topic --brokers 127.0.0.1:9092 list
 go install github.com/pingcap/failpoint/failpoint-ctl@latest
 /go/bin/failpoint-ctl enable
 go test ./...
